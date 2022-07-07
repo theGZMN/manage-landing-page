fe8a4915bc3aefcb175aade2e1a7513c5622a40b
Gozman <thegzmn@gmail.com>
2022-07-07 20:22:52 +0100

    Add custom colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     screens: {
-      sm: '480px',
+      sm: '540px',
       md: '768px',
       lg: '976px',
       xl: '1440px'
@@ -14,8 +14,10 @@ module.exports = {
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)'
       }
     },
   },
