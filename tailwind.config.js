02c2d858ec6ba87aef5707d3458912626ba0ba5b
Gozman <thegzmn@gmail.com>
2022-07-08 01:50:22 +0100

    Import google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,10 @@ module.exports = {
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)'
-      }
+      },
+      fontFamily: {
+        sans: ['Lexend Deca', 'sans-serif']
+      },
     },
   },
   plugins: [],
